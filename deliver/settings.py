46c263d4f8f3daec2eac377b4dea8ede13eeeb89
iammeechar <aldomicha@gmail.com>
2024-01-23 01:51:38 +0300

    Remove Secret key

--- deliver/settings.py
+++ deliver/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#wvltcinkvd@4oce(+c%ypw@aekgsz2@^qv^&_3*z_055h@1x="
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
